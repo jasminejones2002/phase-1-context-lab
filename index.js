c9df15a9cd86863b4737d437c438a39a94b5d9f5
jasminejones2002 <jazziejones02@yahoo.com>
2024-01-12 01:49:28 -0600

    context lab

--- index.js
+++ index.js
@@ -1,5 +1,81 @@
-/* Your Code Here */
+function createEmployeeRecord(array) {
+    const employeeRecord = {};
 
+    employeeRecord.firstName = array[0],
+    employeeRecord.familyName = array[1],
+    employeeRecord.title = array[2],
+    employeeRecord.payPerHour = array[3],
+    
+    employeeRecord.timeInEvents = [],
+    employeeRecord.timeOutEvents = []
+
+    return employeeRecord
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map((array) => createEmployeeRecord(array));
+}
+function createTimeInEvent(dateStamp) {
+    const [date, time] = dateStamp.split(' ')
+
+    const timeInRecord = {
+        type: 'TimeIn',
+        hour: parseInt(time),
+        date: date
+    }
+    this.timeInEvents.push(timeInRecord)
+
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, time] = dateStamp.split(' ')
+
+    const timeOutRecord = {
+        type: 'TimeOut',
+        hour: parseInt(time),
+        date: date
+    }
+
+    this.timeOutEvents.push(timeOutRecord)
+
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date)
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date)
+
+    if (!timeInEvent || !timeOutEvent) {
+        return 0;
+    }
+
+    const timeInHour = parseInt(timeInEvent.hour)
+    const timeOutHour = parseInt(timeOutEvent.hour)
+
+    const hoursWorked = parseInt(timeOutHour - timeInHour) / 100;
+
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(date) {
+    const payOwed = this.payPerHour * hoursWorkedOnDate.bind(this)(date)
+
+    return payOwed;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    const sumOfPay = employeeRecords.reduce((sum, employee) => {
+        const wages = allWagesFor.call(employee);
+        return sum + wages
+    }, 0)
+
+    return sumOfPay;
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
